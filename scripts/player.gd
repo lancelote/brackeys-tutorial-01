84c51ed7e8e4f553c9968fe9e8ddbe6000774893
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-03-20 17:00:39 +0100

    specify explicit `float` type

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const SPEED: float = 130.0
+const JUMP_VELOCITY: float = -300.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
